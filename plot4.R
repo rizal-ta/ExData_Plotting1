f1c02837398847f556eb38765b3190b44b6f1044
rizal-ta <mohamed.rizal@students.iiserpune.ac.in>
2020-08-23 17:40:43 +0530

    correction in plot4.R

--- plot4.R
+++ plot4.R
@@ -38,6 +38,8 @@ with(dat, plot(datetime, Sub_metering_1, type = "l",
 with(dat, points(datetime, Sub_metering_2, type = "l", col = "red"))
 with(dat, points(datetime, Sub_metering_3, type = "l", col = "blue"))
 legend("topright", lwd = c(1,1,1), col = c("black", "red", "blue"), 
+legend("topright", lwd = c(1,1,1), col = c("black", "red", "blue"), bty = "n",
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Plotting Global Reactive Power vs datetime as a line plot
 with(dat, plot(datetime, Global_reactive_power, type = "l"))
